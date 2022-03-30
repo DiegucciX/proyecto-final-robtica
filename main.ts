ad20009ce70789981effc8a540aca4e60835ee62
DiegucciX <98874031+DiegucciX@users.noreply.github.com>
2022-03-30 09:28:15 +0200

    ssdsdf

--- main.ts
+++ main.ts
@@ -7,3 +7,8 @@ input.onButtonPressed(Button.B, function () {
 radio.setGroup(333)
 basic.showString("¡Hola!")
 music.playMelody("F A F D A F D C5 ", 120)
+basic.forever(function () {
+    if (input.rotation(Rotation.Roll) < 30) {
+        radio.sendNumber(4)
+    }
+})
