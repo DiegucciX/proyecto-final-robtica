7b6924ed1aa534674c24aa8632b0355ff1306ee6
DiegucciX <98874031+DiegucciX@users.noreply.github.com>
2022-04-04 11:30:10 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (true) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+    }
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    radio.sendNumber(6)
+})
 input.onButtonPressed(Button.A, function () {
     radio.sendNumber(1)
 })
@@ -10,5 +24,10 @@ music.playMelody("F A F D A F D C5 ", 120)
 basic.forever(function () {
     if (input.rotation(Rotation.Roll) < 30) {
         radio.sendNumber(4)
+    }
+})
+basic.forever(function () {
+    if (input.rotation(Rotation.Roll) > 30) {
+        radio.sendNumber(5)
     }
 })
